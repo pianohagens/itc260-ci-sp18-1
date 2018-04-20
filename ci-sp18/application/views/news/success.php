b5714202c01cf9a138efbe0ef37f48d4062959fd
PianoHagens <pianohagens@gmail.com>
2018-04-19 20:55:12 -0700

    Update .gitignore
    Ignore database.php file

---
+++ ci-sp18/application/views/news/success.php
@@ -0,0 +1,10 @@
+<?php
+//application/views/news/success.php
+?>
+
+<h2>Success Page</h2>
+<h1>Success!</h1>
+<p>This is Success page </p>
+<p><b style="color:red;">A News Item has been Created Successful! </b></p>
+<p>You are currently on the Success page </p>
+<p>You are currently on the Success page </p>
